512465dbb122397856609e9b5a65794cf5e9bb3b
Arthur <arthur@cybrco.ai>
2026-01-15 17:44:23 -0500

    Create bare metal Rust binary

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+/// no_std: Don't include the standard library
+///
+/// The kernel will not have access to the std library binary
+/// because it has operating system constructs for networking,
+/// memory allocation, and file systems, which will need a custom
+/// implementation for Gumbo OS.
+///
+/// no_main: Don't use main as the entry point
+///
+/// The Rust runtime relies on the crt0 library to setup the stack
+/// for a C program. It uses start as the entry point. We will not have
+/// access to this in our kernel.
+
+use core::panic::PanicInfo;
+
+/// Reimplement the panic handler
+///
+/// Normally the Rust std library defines the panic handler for us.
+/// However, since the kernel will not have the std library at start up,
+/// we will need to define our own versions of std library features.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Define a new entrypoint for Gumbo OS
+/// 
+/// no_mangle: Prevents rust from generating a unique id for the function
+/// extern "C": Ensures we use the C calling conventions
+/// It also shouldn't return, and it will rely on an exit system call the OS will implement
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
